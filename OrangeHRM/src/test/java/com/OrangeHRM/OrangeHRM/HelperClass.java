c08cb17b4c33b96adf2774c680dcc6ea54bebdcd
AARTHIES6447 <aarthikumaresan75@gmail.com>
2023-11-02 19:08:17 +0530

    project uploaded

---
+++ OrangeHRM/src/test/java/com/OrangeHRM/OrangeHRM/HelperClass.java
@@ -0,0 +1,45 @@
+package com.OrangeHRM.OrangeHRM;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class HelperClass {
+	public static HelperClass helperclass;
+	private static WebDriver driver;
+	
+	private static WebDriver wait;
+	public final static int TIMEOUT = 10;
+	
+	//constructor
+	HelperClass(){
+		driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
+		driver.manage().window().maximize();
+	}
+	public static void openPage(String url) {
+		driver.get(url);		
+	}
+	
+	public static WebDriver getDriver() {
+		return driver;
+	}
+	
+	public static void setUpDriver() {
+		if(helperclass == null) {
+			helperclass = new HelperClass();			
+		}
+	}
+	
+	public static void tearDown() {
+		if(driver!=null) {
+			driver.close();
+			driver.quit();
+		}
+		helperclass = null;
+	}
+	
+
+}
